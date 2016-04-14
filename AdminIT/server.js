54b843a2cfeca7fc80cdc2839abde90d3650b3ed
DamnMusz <damianfiuba@gmail.com>
2016-04-13 21:57:42 -0300

    herokuando2

--- AdminIT/server.js
+++ AdminIT/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json())
 
 app.use(cookieParser());
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address
     var port = server.address().port
     

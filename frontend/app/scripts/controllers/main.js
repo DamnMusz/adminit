05e5125d29d6d2a34631219aaa7cc4e2814aae4c
Facundo Tkaczyszyn <f.tkaczyszyn@globant.com>
2016-05-05 11:35:56 -0300

    super archi simple $http get from main controller

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the adminitApp
  */
 angular.module('adminitApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.echo = function() {
+            $http.get("http://urlecho.appspot.com/echo?status=200&Content-Type=text%2Fhtml&body=Hello%20world!").
+            then(function(response){
+                $scope.echoMessage = response.data;
+            });
+        };
+
+    });

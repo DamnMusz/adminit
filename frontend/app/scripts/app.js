f6e5f7bc80d4a95cdbfb351c4949253d3dd95211
facu.tk <facu.tk@gmail.com>
2016-05-06 19:38:19 -0300

    Material Design Lite

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -17,6 +17,35 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+
+
+  .run(function () {
+    var mdlUpgradeDom = false;
+    setInterval(function() {
+      if (mdlUpgradeDom) {
+        componentHandler.upgradeDom();
+        mdlUpgradeDom = false;
+      }
+    }, 200);
+
+    var observer = new MutationObserver(function () {
+      mdlUpgradeDom = true;
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+    /* support <= IE 10
+    angular.element(document).bind('DOMNodeInserted', function(e) {
+        mdlUpgradeDom = true;
+    });
+    */
+  })
+
+  // .config(function(mdlConfigProvider){
+  //   mdlConfigProvider.floating = false;
+  //   mdlConfigProvider.rippleEffect = false;
+  // })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -11,9 +11,22 @@ angular.module('adminitApp')
     .controller('MainCtrl', function ($scope, $http) {
         $scope.cards = [];
 
-        $http.get("http://api.adminit.ml/cards").
-        then(function(response){
+        $http.get("http://api.adminit.ml/cards")
+        .then(function(response){
             $scope.cards = response.data;
         });
+
+        $scope.delete = function(card) {
+            $http.delete("http://api.adminit.ml/cards/" + card.id)
+            .then(function(response){
+                var cardIndex = 0;
+                for (var cardIndex = 0; cardIndex < $scope.cards.length; cardIndex++) {
+                    if ( $scope.cards[cardIndex].id == card.id ) {
+                        $scope.cards.splice(cardIndex, 1);
+                        break;
+                    }
+                }
+            });
+        };
 
     });

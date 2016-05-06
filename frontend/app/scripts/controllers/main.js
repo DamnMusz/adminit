cae2619766304ec7d1800cccb2b3a8041d064a46
facu.tk <facu.tk@gmail.com>
2016-05-06 11:02:21 -0300

    landing conectected to backend

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -9,17 +9,11 @@
  */
 angular.module('adminitApp')
     .controller('MainCtrl', function ($scope, $http) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+        $scope.cards = [];
 
-        $scope.echo = function() {
-            $http.get("http://api.adminit.ml/cards").
-            then(function(response){
-                $scope.echoMessage = response.data;
-            });
-        };
+        $http.get("http://api.adminit.ml/cards").
+        then(function(response){
+            $scope.cards = response.data;
+        });
 
     });
